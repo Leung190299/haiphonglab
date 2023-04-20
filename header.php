e2ff07f232d91eaef819071e0412b2a8fed843f5
Leung190299 <leung01628381807@gmail.com>
2023-04-21 00:02:10 +0700

    create header

--- header.php
+++ header.php
@@ -13,13 +13,58 @@
     <?php wp_body_open(); ?>
 
     <!-- nội dung header -->
-    <?php wp_nav_menu(
-        [
-            'theme_location' => 'menu',
-            'container' => 'false',
-            'menu_id' => 'header-menu',
-            'menu_class' => 'menu'
-        ]
-    ); ?>
+    <header class="header">
+        <div class="header_top">
+            <div class="container">
+                <div class="header_body">
+                    <div class="header_logo">
+                        <?php the_custom_logo() ?>
+                    </div>
+                    <div class="header_menu">
+                        <?php
+                        wp_nav_menu([
+                            'theme_location' => 'menu',
+                            'menu_class' => 'header_menuBody',
+                        ]); ?>
+                        <div class="header_search">
+                            <button><?php Template_function::getIcon('search') ?></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header_bottom">
+            <div class="container">
+                <div class="header_body">
+                    <div class="header_boxItem">
+                        <button class="header_item">
+                            <div class="header_itemIcon">
+                                <?php Template_function::getIcon('calendar') ?>
+                            </div>
+                            <div class="header_itemTitle">ĐẶT LỊCH HẸN</div>
+                        </button>
+                        <button class="header_item">
+                            <div class="header_itemIcon">
+                                <?php Template_function::getIcon('page') ?>
+                            </div>
+                            <div class="header_itemTitle">XEM KẾT QUẢ XÉT NGHIỆM</div>
+                        </button>
+                        <button class="header_item">
+                            <div class="header_itemIcon">
+                                <?php Template_function::getIcon('edit') ?>
+                            </div>
+                            <div class="header_itemTitle">LIÊN HỆ GÓP Ý</div>
+                        </button>
+                        <button class="header_item">
+                            <div class="header_itemIcon">
+                                <?php Template_function::getIcon('phone') ?>
+                            </div>
+                            <div class="header_itemTitle">HOTLINE: 0915 82 1509</div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <main class="main" role="main">
--- src/TemplateFunction.php
+++ src/TemplateFunction.php
@@ -2,15 +2,15 @@
 
 class Template_function {
 
-	public static function nvn_get_image_path( $name ) {       ?>
+	public static function getImage( $name ) {       ?>
 		<img class="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/<?php echo esc_attr( $name ) ?>" alt="">
 		<?php
 	}
-	public static function nvn_icon( $name ) {
-		 $icon = file_get_contents( get_template_directory() . "/images/$name.svg" );
+	public static function getIcon( $name ) {
+		 $icon = file_get_contents( get_template_directory() . "/images/icons/$name.svg" );
 		echo $icon;
 	}
-	public static function nvn_get_image_id( $ID ) {
+	public static function getImageId( $ID ) {
 		?>
 		<div class="entry-thumbnail">
 			<?php echo wp_get_attachment_image( $ID, 'full', false ) ?>
