ef4f68e581ed973d238cfe3f6983acd98f70b7c9
dongthuythanh <thanhthanhdong2003@gmail.com>
2023-04-29 15:12:54 +0700

    create service

---
+++ page-templates/service-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Service page
+ */
+?>
+<?php get_header(); ?>
+
+<main id="primary" class="site-main ">
+
+	<?php
+		get_template_part('template-parts/home-page/banner');
+		get_template_part('template-parts/home-page/getTest');
+		get_template_part('template-parts/home-page/service');
+		get_template_part('template-parts/home-page/homePost');
+
+	?>
+
+</main>
+
+<?php get_footer(); ?>
