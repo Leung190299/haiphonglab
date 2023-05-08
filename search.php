55d5fa2f77dd8206ffda3318cf3e6039f72c5d88
dongthuythanh <thanhthanhdong2003@gmail.com>
2023-05-09 00:21:31 +0700

    update search

--- search.php
+++ search.php
@@ -40,24 +40,3 @@ get_header();
 get_footer();
 ?>
 
-<script>
-	const elements = document.querySelectorAll('.search-body');
-
-function fadeInElements() {
-elements.forEach(element => {
-	const elementTop = element.getBoundingClientRect().top;
-	const elementHeight = element.offsetHeight;
-	const windowHeight = window.innerHeight;
-
-	if (elementTop < windowHeight - elementHeight) {
-	element.style.opacity = '1';
-	}
-});
-}
-
-window.addEventListener('scroll', fadeInElements);
-window.addEventListener('resize', fadeInElements);
-
-
-fadeInElements();
-</script>
